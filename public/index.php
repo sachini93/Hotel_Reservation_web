c937f86b0ba828e3ac19b8e816f8c4c9236097f1
sachini93 <sachini.thaksala@gmail.com>
2017-08-06 07:13:57 -0700

    initial structure

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reservation</title>
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/body.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/nav.css">
+    <link rel="stylesheet" href="../css/user.css">
+</head>
+<body bgcolor="#cc99ff">
+    <div class="main" style="width: 100%; height:1300px; background-color: #cc66ff; border: dotted" >
+        <div class="top">
+            <marquee styl>Hotel Levendra</marquee>
+        </div>
+        <div class="user">
+            <div clas="user" id="login_logout">
+                <a href="login.php">Login</a>
+            </div>
+            <div class="user" id="user_img">
+                <img src="../images/user1.jpg">
+            </div>
+        </div>
+        <div class="nav">
+
+        </div>
+        <div class="body">
+
+        </div>
+        <div class="footer">
+
+        </div>
+    </div>
+</body>
+</html>
+
